6684eb44a6248a767e1a7e274b7575ee8a4348fd
Ido Kadosh <1234kadosh@gmail.com>
2024-06-10 13:00:39 +0300

    feat: add ExpandableTextArea to ChatIndex

--- src/pages/ChatIndex.tsx
+++ src/pages/ChatIndex.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
+import ExpandableTextarea from '../cmps/ExpandableTextArea';
 
 const ChatIndex = () => {
-	return <div>ChatIndex</div>;
+	const [question, setQuestion] = useState<string>('');
+
+	const onAskQuestion = async (ev: React.FormEvent) => {
+		ev.preventDefault();
+		if (!question) return;
+	};
+
+	return (
+		<div>
+			<form onSubmit={onAskQuestion}>
+				<div className="flex gap-2 bg-gray-200 mx-auto mt-2 rounded-[26px] py-2 px-2 text-gray-700 focus:outline-none max-w-[48rem]">
+					<ExpandableTextarea value={question} onChange={setQuestion} placeholder="Ask Judge Judi" />
+					<button className="bg-primary text-white rounded-full px-4 py-2 self-center">Ask</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default ChatIndex;
